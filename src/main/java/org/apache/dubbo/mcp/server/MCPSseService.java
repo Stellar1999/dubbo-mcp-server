edce2e5ab01f87fcc390651c76937d75e54c2fff
Stellar <stellar.yu1999@gmail.com>
2025-05-15 15:02:07 +0800

    This is the first draft of the message and SSE implementation.No tool register.

--- src/main/java/org/apache/dubbo/mcp/server/MCPSseService.java
+++ src/main/java/org/apache/dubbo/mcp/server/MCPSseService.java
@@ -1,14 +1,13 @@
 package org.apache.dubbo.mcp.server;
 
-import com.alibaba.fastjson2.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.modelcontextprotocol.server.McpAsyncServer;
 import io.modelcontextprotocol.server.McpServer;
 import jakarta.annotation.PostConstruct;
-import org.apache.dubbo.common.beanutil.JavaBeanSerializeUtil;
 import org.apache.dubbo.common.stream.StreamObserver;
 import org.apache.dubbo.config.annotation.DubboService;
-import org.apache.dubbo.mcp.server.mcp.DubboMcpSseTransportProvider;
+import org.apache.dubbo.mcp.server.transport.DubboMcpSseTransportProvider;
+import org.apache.dubbo.remoting.http12.message.ServerSentEvent;
 
 
 @DubboService
@@ -21,12 +20,12 @@ public class MCPSseService implements McpService{
     private final DubboMcpSseTransportProvider transportProvider = getTransportProvider();
 
     @Override
-    public void get(StreamObserver<String> responseObserver) {
+    public void get(StreamObserver<ServerSentEvent<String>> responseObserver) {
         transportProvider.handleRequest(responseObserver);
     }
 
     @Override
-    public void post(StreamObserver<String> responseObserver) {
+    public void post(StreamObserver<ServerSentEvent<String>> responseObserver) {
         transportProvider.handleRequest(responseObserver);
     }
 
--- src/main/java/org/apache/dubbo/mcp/server/McpService.java
+++ src/main/java/org/apache/dubbo/mcp/server/McpService.java
@@ -2,12 +2,17 @@ package org.apache.dubbo.mcp.server;
 
 
 import org.apache.dubbo.common.stream.StreamObserver;
+import org.apache.dubbo.remoting.http12.message.ServerSentEvent;
 import org.apache.dubbo.remoting.http12.rest.Mapping;
+import org.apache.dubbo.remoting.http12.rest.Param;
+import org.apache.dubbo.remoting.http12.rest.ParamType;
+
+@Mapping("")
 public interface McpService{
 
     @Mapping("/mcp/sse")
-    void get(StreamObserver<String> responseObserver);
+    void get(StreamObserver<ServerSentEvent<String>> responseObserver);
 
     @Mapping("/mcp/message")
-    void post(StreamObserver<String> responseObserver);
+    void post(StreamObserver<ServerSentEvent<String>> responseObserver);
 }
